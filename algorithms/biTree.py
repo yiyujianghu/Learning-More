27d86910d58d6245204e91ef2408d293524512b9
yiyujianghu <dongjunyou@126.com>
2019-04-08 18:07:16 +0800

    add first search

--- algorithms/biTree.py
+++ algorithms/biTree.py
@@ -6,7 +6,7 @@
 @time: 2019/4/8 11:09
 """ 
 
-
+from collections import deque
 
 class treeNode():
     """先构建一个树节点"""
@@ -83,30 +83,51 @@ class Tree():
 
     def breadthFirstSearch(self, node):
         """宽度优先遍历，层次遍历"""
-        pass
+        if not node:
+            return
+        else:
+            nodeQueue = deque()
+            nodeQueue.append(node)
+            while nodeQueue:
+                cursor = nodeQueue.popleft()
+                print(cursor.val, end=" ")
+                if cursor.left:
+                    nodeQueue.append(cursor.left)
+                if cursor.right:
+                    nodeQueue.append(cursor.right)
 
     def depthFirstSearch(self, node):
         """深度优先遍历"""
-        pass
+        if not node:
+            return
+        else:
+            nodeStack = []
+            nodeStack.append(node)
+            while nodeStack:
+                cursor = nodeStack.pop()
+                print(cursor.val, end=" ")
+                if cursor.right:
+                    nodeStack.append(cursor.right)
+                if cursor.left:
+                    nodeStack.append(cursor.left)
 
     @staticmethod
     def orderPrint(func):
-        orderList = {"preOrder": "前序遍历: ", "inOrder": "中序遍历: ", "backOrder": "后序遍历: "}
-        print(orderList[func.__name__], end=" ")
+        orderList = {"preOrder": "前序遍历: ", "inOrder": "中序遍历: ", "backOrder": "后序遍历: ",
+                     "breadthFirstSearch": "宽度优先遍历", "depthFirstSearch": "深度优先遍历"}
+        print(orderList[func.__name__], end="")
         func(tree.root)
-        print("-"*3+">")
+        print("")
 
 
 def createTree(L):
     """简单的构建一棵完全二叉树"""
     tree = Tree()
     for x in L:
-        print(x)
         tree.addNode(x)
+    tree.root.left.left.left = treeNode(7)   # 这里简单的加入一个孤立节点
     tree.root.left.right.right = treeNode(10)   # 这里简单的加入一个孤立节点
     return tree
-
-
 
 
 if __name__ == "__main__":
@@ -116,5 +137,7 @@ if __name__ == "__main__":
     Tree.orderPrint(tree.preOrder)
     Tree.orderPrint(tree.inOrder)
     Tree.orderPrint(tree.backOrder)
+    Tree.orderPrint(tree.breadthFirstSearch)
+    Tree.orderPrint(tree.depthFirstSearch)
 
 
