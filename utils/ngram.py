082b24697819480bc1f87ec09a24681b6cc68a19
yiyujianghu <dongjunyou@126.com>
2019-08-23 16:54:14 +0800

    ngram model

---
+++ utils/ngram.py
@@ -0,0 +1,119 @@
+# coding : utf-8
+
+import jieba
+import numpy as np
+
+class NGram():
+    def __init__(self, sentence):
+        self.sentence = sentence
+
+    def probSearch(self, subDict, wordList):
+        try:
+            probability = self.probSearch(subDict[wordList[0]], wordList[1:])
+            if len(wordList) > 1:
+                return probability
+            else:
+                return subDict[wordList[0]]["prob"]
+        except:
+            return -100
+
+
+    def detectERROR(self, dictProb, N):
+        stopList = ["，", "。", "！", "："]
+        sentenceList = [word for word in jieba.cut(self.sentence) if word not in stopList]
+        print(sentenceList)
+        print("".join(sentenceList))
+        errIndex = []
+        for i in range(len(sentenceList)-N):
+            wordList = sentenceList[i: i+N]
+            if wordList[0] not in dictProb:
+                errIndex.append(i)
+                sentenceList[i] = "\033[0;31m{}\033[0m".format(sentenceList[i])
+                continue
+            probability = self.probSearch(dictProb[wordList[0]], wordList[1:])
+            if probability < -20:
+                errIndex.append(i+3)
+                sentenceList[i+3] = "\033[0;31m{}\033[0m".format(sentenceList[i+3])
+        print("".join(sentenceList))
+        return errIndex
+
+
+    @classmethod
+    def rmStopword(cls, data):
+        stopList = ["，", "。", "！", "："]
+        for i in range(len(data)):
+            line = []
+            for w in data[i].split(" "):
+                if w not in stopList:
+                    line.append(w)
+            data[i] = line
+        return data
+
+
+    @classmethod
+    def loadStopList(cls):
+        with open("../../data/dict/stopdict.txt", "r", encoding="utf-8") as f:
+            stopList = []
+            for w in f.readlines():
+                stopList.append(w.replace("\n", ""))
+        return stopList
+
+
+    @classmethod
+    def addLine(cls, currNode, wordList, N):
+        for i in range(len(wordList)):
+            cls.addNode(currNode, wordList[i:], N)
+
+
+    @classmethod
+    def addNode(cls, currNode, wordList, N):
+        if wordList[0] not in currNode:
+            currNode[wordList[0]] = {"count": 1}
+        else:
+            currNode[wordList[0]]["count"] += 1
+        if len(wordList[1:N]) > 0:
+            cls.addNode(currNode[wordList[0]], wordList[1:N], N - 1)
+
+
+    @classmethod
+    def calOneCount(cls, dictStatic):
+        oneCountList = [dictStatic[k]["count"] for k in dictStatic.keys()]
+        oneCount = np.array(oneCountList).sum()
+        return oneCount
+
+
+    @classmethod
+    def caculateProb(cls, dictStatic):
+        dictProb = {}
+        summary = dictStatic.pop("count") if dictStatic["count"] > 0 else 1
+        for k in dictStatic.keys():
+            dictProb[k] = {"prob": np.log(dictStatic[k]["count"] / summary)}
+            subDict = cls.caculateProb(dictStatic[k])
+            if len(subDict) > 0:
+                dictProb[k].update(subDict)
+        return dictProb
+
+
+    @classmethod
+    def train(cls, data, N):
+        data = cls.rmStopword(data)
+        dictStatic = {}
+        for line in data:
+            cls.addLine(dictStatic, line, N)
+        print(dictStatic)
+        dictStatic["count"] = cls.calOneCount(dictStatic)
+        dictProbability = cls.caculateProb(dictStatic)
+        dictProbability["prob"] = 0
+        return dictProbability
+
+
+if __name__ == "__main__":
+    data = ["为了祖国，为了胜利，向我开炮！向我开炮！",
+            "记者：你怎么会说出那番话，我只是觉得",
+            "我只是觉得，对准我自己打"]
+    data = [" ".join(jieba.lcut(w)) for w in data]
+    dictProbability = NGram.train(data, 3)
+    print(dictProbability)
+    sentence = "为了祖国火炬，向我开炮，向我开炮，向我开炮的大战略，我只是觉得，对准我自己打"
+    example = NGram(sentence)
+    example.detectERROR(dictProbability, 3)
